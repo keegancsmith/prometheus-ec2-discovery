82d01cfa2cb7720516c28c64f9255ba700b5bc55
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2015-08-07 17:38:12 +0200

    prometheus-update-config.sh to glue restarting prometheus to gen script

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ prometheus-ec2-gen
 
 Proof of concept for generating `prometheus.yml` files based on instances in
 EC2.
+
+<code>
+PROMETHEUS_CONFIG_GEN="go run prometheus-ec2-gen.go" bash -x ./prometheus-update-config.sh
+</code>
---
+++ prometheus-update-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Generates a new prometheus.yml and reloads prometheus if the contents
+# change. Intended to be run from a cronjob
+
+gen_cmd=${PROMETHEUS_CONFIG_GEN:=prometheus-ec2-gen}
+config_path=${PROMETHEUS_CONFIG_PATH:=prometheus.yml}
+config_path_new=${config_path}.prospective
+
+set -e
+${gen_cmd} > ${config_path_new}
+set +e
+
+if [[ $(shasum - < ${config_path}) != $(shasum - < ${config_path_new}) ]]; then
+    mv ${config_path} ${config_path}.bak
+    mv ${config_path_new} ${config_path}
+    logger -s -p info -t prometheus-update-config.sh "Updated ${config_path}, reloading prometheus..."
+    pkill -SIGHUP prometheus
+else
+    rm ${config_path_new}
+fi
